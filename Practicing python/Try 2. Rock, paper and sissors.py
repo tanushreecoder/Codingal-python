c5dcd194469b6c09e7a104b670ab91d80a1788e3
tanushreecoder <tanushree.coder@gmail.com>
2025-08-27 21:17:30 +0600

    OOP's HW 1

---
+++ Practicing python/Try 2. Rock, paper and sissors.py
@@ -0,0 +1,8 @@
+print("Do you want to play rock, paper, sissors?")
+print("We'll do 5 rounds and the winner will be the person with more points")
+import random
+1 = "rock"
+2 = "sissors"
+3 = "paper"
+x = random.randint(1, 4)
+y = input("What is your first choise?")
