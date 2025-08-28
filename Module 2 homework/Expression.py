86b892408854adf5072826f6a3c56825b73d45b2
tanushreecoder <tanushree.coder@gmail.com>
2025-08-28 22:10:43 +0600

    Module 2 HW

---
+++ Module 2 homework/Expression.py
@@ -0,0 +1,19 @@
+print("So, you will enter 3 numbers and we will first sum the first two numbers")
+print("Then we'll get that number, and subract the 3rd num from it")
+print("After that, we will multiply that num with the 2nd num and lastly...")
+print("We will divide the first num with that num.")
+print("Let the chaos begin.")
+x = float(input("What is your 1st num?"))
+y = float(input("And second?"))
+z = float(input("Third?"))
+
+print(f"So your nums are {x}, {y}, {z}")
+
+a = x + y
+b = a - z
+c = b * y
+d = c / x
+
+print(f"The resault of chaos is...{d}")
+
+#I was not sure what I was supposed to do, but I saw something about expressions. So, I tried my best to come up with something and this is what it is, I did not put any OOPs because I wasn't sure where should I apply it. And, I hope this is good :D
